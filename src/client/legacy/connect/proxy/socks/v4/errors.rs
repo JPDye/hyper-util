5fde51d6c71d7fa2f738cd313b327748b6655580
Joe <jpzh.dye@gmail.com>
2025-04-24 15:46:39 +0000

    .

--- src/client/legacy/connect/proxy/socks/v4/errors.rs
+++ src/client/legacy/connect/proxy/socks/v4/errors.rs
@@ -1,2 +1,12 @@
+use super::Status;
+
 #[derive(Debug)]
-pub enum SocksV4Error {}
+pub enum SocksV4Error {
+    Command(Status),
+}
+
+impl From<Status> for SocksV4Error {
+    fn from(err: Status) -> Self {
+        Self::Command(err)
+    }
+}
